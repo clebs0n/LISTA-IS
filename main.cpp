11c00a92956b3bdfe9d07edb24ba1f51eab09126
clebs0n <95928023+clebs0n@users.noreply.github.com>
2022-09-15 14:17:14 -0300

    q1

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 #include <string.h>
 
 int * votos;
-int cont, numArq;
+int cont, numArq, numCdt;
 pthread_mutex_t * mutexCdt = NULL; 
 pthread_mutex_t fileFlag = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct{
+    int winnerIdx;
+    int winnerPoints;
+} winner;
 
 void * funcao(void * args){
   char voto[10];
@@ -21,13 +26,17 @@ void * funcao(void * args){
     pthread_mutex_unlock(&fileFlag);
     n=0;
 
-  while (fscanf(archive[i], "%s", voto) != -1) {
+  while (fscanf(archive[i], "%s", voto) != -1 && n < numCdt) {
     if(voto[0]!=48){
       pthread_mutex_lock(&mutexCdt[n]);
       votos[n]++;
       pthread_mutex_unlock(&mutexCdt[n]);
       }
-      //printf("%s", voto);
+      if(voto[0]==48){
+        pthread_mutex_lock(&mutexCdt[numCdt]);
+        votos[numCdt]++;
+        pthread_mutex_unlock(&mutexCdt[numCdt]);
+      }
       n++;
     }
   }
@@ -35,28 +44,27 @@ void * funcao(void * args){
   }
 
 void changeName(char * fileName, int n){
-  sprintf(fileName, "../candidatos/%d.in.txt", n);
+  sprintf(fileName, "%d.in", n);
 }
 
 int main(){
   int arq, trd, cdt, n=1;
-  char file[22];
+  winner winner;
+  float porcentagem=0, totalVotos=0;
+  winner.winnerPoints = 0;
+  char file[100];
   scanf("%d %d %d", &arq, &trd, &cdt);
-  votos = (int*)malloc(sizeof(int)*cdt);
-  mutexCdt= (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*cdt);
+  votos = (int*)malloc(sizeof(int)*(cdt+1));
+  mutexCdt= (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*(cdt+1));
 
-  
-  for(int i=0;i<cdt;i++){
+  for(int i=0;i<=cdt;i++){
     mutexCdt[i] = PTHREAD_MUTEX_INITIALIZER;
   }
-  
-  cont = 0;
-  numArq = arq;
+  cont = 0;numArq = arq;numCdt = cdt;
 
   FILE *arqvs[arq];
   for(int i=1, count=0;i<=arq;i++, count++){
     changeName(file, i);
-    //printf("-%s-\n", file);
     arqvs[count] = fopen(file, "r");
     
     if(arqvs[count]==NULL){
@@ -64,20 +72,34 @@ int main(){
       exit(1);
     }
   }
-
   pthread_t thread[trd];
   for(int i=0;i<trd; i++){
+    //printf("Thread:%d\n", i);
     pthread_create(&thread[i], NULL, funcao, &arqvs);
   }
 
   for(int i = 0; i < trd; i++){
         pthread_join(thread[i], NULL);
   }
-  //funcao(&arqvs);
-  printf("ba");
- for(int i=0;i<cdt;i++){
-    printf("%d", votos[i]);
+  for(int i = 0; i < cdt; i++){totalVotos += votos[i];}
+ for(int i=0;i<=cdt;i++){
+    if(i!=cdt){
+    porcentagem = (votos[i]/totalVotos)*100;
+    printf("Candidato : %d | Votos : %.0f%c\n", i+1, porcentagem, 37);
+    }
+    else{
+        printf("Brancos : %d\n", votos[numCdt]);
+    }
+    if(votos[i] > winner.winnerPoints && i!= numCdt){
+        winner.winnerPoints = votos[i];
+        winner.winnerIdx = i;
+    }
   }
+  printf("\n\nVencedor : %d\n\n", winner.winnerIdx+1);
+
+  free(votos);
+  free(mutexCdt);
   
   return 0;
 }
+  
